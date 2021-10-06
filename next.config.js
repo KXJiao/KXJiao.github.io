7929f84e5ac734c4db91844536f266faac822295
Kevin Jiao <kxjiao@gmail.com>
2021-10-05 23:15:33 -0400

    Update third party loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 // next.config.js
-const withPlugins = require('next-compose-plugins');
-const optimizedImages = require('next-optimized-images');
+const withOptimizedImages = require('next-optimized-images');
 
-module.exports = withPlugins([
-  [optimizedImages, {}],
-]);
+module.exports = withOptimizedImages([]);
