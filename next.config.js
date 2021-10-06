512d323b501748ea559e85fa20fecd004b907997
Kevin Jiao <kxjiao@gmail.com>
2021-10-05 22:58:51 -0400

    Install third party loader for image optimization

---
+++ next.config.js
@@ -0,0 +1,7 @@
+// next.config.js
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
+module.exports = withPlugins([
+  [optimizedImages, {}],
+]);
