1a49a504c64b5f264e2aaa1a204249d4c8fbe4d9
Kevin Jiao <kxjiao@gmail.com>
2022-02-16 03:53:19 -0500

    Create constants file

---
+++ constants/info.tsx
@@ -0,0 +1,10 @@
+const userInfo = {
+  name: "Kevin Jiao",
+  siteTitle: "Kevin Jiao's Site",
+  socialLinks: {
+    linkedin: "https://linkedin.com/in/kxjiao",
+    github: "https://github.com/KXJiao",
+  },
+};
+
+export default userInfo;
