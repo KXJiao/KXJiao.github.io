b4dd2d082f4ece6863ac676733e781197df72b38
Kevin Jiao <kxjiao@gmail.com>
2022-02-17 18:41:20 -0500

    Add Logo and home button to navbar and shift links right

--- constants/info.ts
+++ constants/info.ts
@@ -2,6 +2,7 @@ const userInfo = {
   name: "Kevin Jiao",
   siteTitle: "Kevin Jiao",
   metaPic: "https://og-image.vercel.app/**Kevin%20Jiao**.png?theme=dark&md=1&fontSize=225px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-white-logo.svg&images=https%3A%2F%2Fraw.githubusercontent.com%2Ftwitter%2Ftwemoji%2Fba9c621af05213b19107d27a4b60afed888033dc%2Fsvg%2F1f414.svg&images=https%3A%2F%2Fraw.githubusercontent.com%2Fgooglefonts%2Fnoto-emoji%2F9a5261d871451f9b5183c93483cbd68ed916b1e9%2Fsvg%2Femoji_u1f99c.svg&widths=350&widths=350&widths=350&heights=350&heights=350&heights=350",
+  siteLinks: ["About", "Projects", "Misc"],
   projects: [
     {
       imageURL: "/images/Change1.PNG",
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.info("Chicken emoji from Twemoji: https://twemoji.twitter.com/");
   return (
     <ChakraProvider>
       <Component {...pageProps} />
