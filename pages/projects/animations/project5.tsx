43df6b8328d963465bf454ed8f467358e26546a5
Kevin Jiao <kxjiao@gmail.com>
2022-04-28 10:22:33 -0400

    Update project 5 with animation and images

--- pages/projects/animations/project5.tsx
+++ pages/projects/animations/project5.tsx
@@ -22,42 +22,65 @@ const Project5 = () => {
         <Heading size="md">Final Animation:</Heading>
         <ArticleVideo
           size="80vw"
-          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-        />
-
-        <Spacer />
-        <Heading size="md">Storyboard</Heading>
-        <Image
-          h={800}
-          src="/images/animations/project5/storyboard.png"
-          alt="Python scripting animation storyboard"
+          src="https://www.youtube.com/embed/jxse_iZ4uW0"
         />
 
         <Spacer />
 
-        <Heading size="md">Words</Heading>
-        <Heading size="sm">Wow</Heading>
+        <Heading size="md">Images</Heading>
+        <Heading size="sm">Dominos Image</Heading>
         <SimpleGrid justifyItems="center" columns={2} spacing={5}>
           <Image
             h={64}
-            src="/images/animations/project5/picture.png"
-            alt="image"
+            src="/images/animations/project5/Duke.png"
+            alt="Java Duke t-posed on blue screen"
           />
           <Text justifySelf="start" fontSize="lg">
-            They call me Lorrem Ipsum Dolor Sit Amet
+            The image used on the dominos. Did you know that Java has a mascot,
+            named Duke? The developers even 3d modeled him in various
+            capacities.
+          </Text>
+        </SimpleGrid>
+        <Heading size="sm">Dominos</Heading>
+        <SimpleGrid justifyItems="center" columns={2} spacing={5}>
+          <Image
+            h={64}
+            src="/images/animations/project5/icon.png"
+            alt="Dominos before falling"
+          />
+          <Text justifySelf="start" fontSize="lg">
+            3 Billion Devices Run Log4j
+          </Text>
+        </SimpleGrid>
+        <Heading size="sm">Background Image</Heading>
+        <SimpleGrid justifyItems="center" columns={2} spacing={5}>
+          <Image
+            h={64}
+            src="/images/animations/project5/windows.png"
+            alt="Bliss background 4k"
+          />
+          <Text justifySelf="start" fontSize="lg">
+            Windows XP
           </Text>
         </SimpleGrid>
 
         <Spacer />
         <Heading size="md">Notes</Heading>
         <Text justifySelf="start" fontSize="lg">
-          Notes
+          If someone could let me know why there exists a 3d Model of Duke
+          driving a car, that would be great
         </Text>
         <Spacer />
 
         <Heading size="md">Credits</Heading>
-        <Link href="https://www.youtube.com/watch?v=gy1B3agGNxw" isExternal>
-          Credits by Credits
+        <Link
+          href="http://cr.openjdk.java.net/~jeff/Duke/3d-preview/Duke.png"
+          isExternal
+        >
+          Duke from OpenJDK Duke Project
+        </Link>
+        <Link href="https://github.com/openjdk/duke/tree/master/3D" isExternal>
+          Duke Car Bottom Text from openjdk's Github
         </Link>
       </Article>
     </Layout>
